62137577ba117905056a15cc4132d3ee4787aaec
Oz <osvaldo.lopez@superluminal.me>
2012-10-01 20:54:49 -0600

    Lots of changes to the Event system.

--- application/pre_load/pre_controller/01_Events.php
+++ application/pre_load/pre_controller/01_Events.php
@@ -1,36 +1,47 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Register and process event handlers.
+ */
 class Event_Handler {
+	/**
+	 * Registered hooks
+	 * @var array
+	 */
 	private static $hooks = array();
 
+	/**
+	 * Include all of the files in $folder. Run the register method of the object included.
+	 * @param string $folder
+	 */
 	public static function load_hooks( $folder = '' ) {
 		$path  = APPPATH . 'libraries/hooks/' . $folder;
 		$path .= substr($path, -1) == '/' ? '' : '/';
 
 		if ( is_dir($path) && $dir = opendir($path)) {
 			while ( ($file = readdir($dir)) !== false ) {
-				if ( !in_array($file, array('.', '..')) && substr($file, -3) == 'php') {
+				if ( substr($file, -3) == 'php') {
+					// Only register the hooks if the file has not been included before.
 					if ( !in_array($path.$file, get_included_files()) ) {
 						include_once $path.$file;
 
-						// To avoid class name collision, we'll require that classes be named folder_filename
+						// To avoid class name collision, we'll require that classes be named <folder>_<filename>
 						$class = $folder.'_'.str_replace('.php', '', $file);
 						if (class_exists($class) && method_exists($class, 'register')) {
-							call_user_func(array($class, 'register'));
+							$class::register();
 						}
 					}
 				}
 			}
 			closedir($dir);
 		}
-
-		unset( $path, $dir, $file );
 	}
 
 	/**
-	 * Register a callback for an event.
+	 * Register a callback for an event. Advise is the state of the event.
+	 * Be default this is either BEFORE or AFTER, but custom advisements may be created.
 	 * @param string $event			The name of the event we want to listen to
-	 * @param string $advise		BEFORE or AFTER
+	 * @param string $advise		BEFORE or AFTER (or custom)
 	 * @param callable $callback	callable
 	 * @param mixed $params			params
 	 */
@@ -41,38 +52,59 @@ class Event_Handler {
 		);
 	}
 
+	/**
+	 * Register a callback for BEFORE the event.
+	 * @param string $event			The name of the event we want to listen to.
+	 * @param callable $callback	php callable
+	 * @param mixed $params			Additional parameters we want our callback to receive
+	 */
 	public static function register_before( $event, $callback, $params = null ) {
 		self::register($event, Event::BEFORE, $callback, $params);
 	}
 
+	/**
+	 * Register a callback for AFTER the event.
+	 * @param string $event			The name of the event we want to listen to.
+	 * @param callable $callback	php callable
+	 * @param mixed $params			Additional parameters for our callback
+	 */
 	public static function register_after( $event, $callback, $params = null ) {
 		self::register($event, Event::AFTER, $callback, $params);
 	}
 
+	/**
+	 * Call every hook attached to an advisement. Generally BEFORE or AFTER,
+	 * however custom states can be created.
+	 * @param string $event		The name of the event to process
+	 * @param string $advise	The state of the event. Generally BEFORE or AFTER
+	 */
 	public static function process_event( &$event, $advise='' ) {
-		$event_name = $event->name . ($advise ? '_'.$advise : '_'.Event::BEFORE);
+		$event->_continue = true; // Reset the continue flag
 
+		// Run hooks
+		$event_name = $event->name . ($advise ? '_'.$advise : '_'.Event::BEFORE);
 		if ( !empty(self::$hooks[$event_name]) ) {
 			foreach ( self::$hooks[$event_name] as $hook ) {
-				$callback = $hook['callback'];
-				$params   = $hook['params'];
+				$callback = !empty($hook['callback']) ? $hook['callback'] : null;
+				$params   = !empty($hook['params']) ? $hook['params'] : null;
 
+				// Anonymous function
 				if ( $callback instanceof Closure ) {
-					$callback( $event, $params );
+					$event->result = $callback( $event, $params );
 				}
-
+				// Object & Static methods
 				else if ( is_array($callback) ) {
 					list($obj, $method) = $callback;
 
 					if ( is_string($obj) ) {
-						$obj::$method( $event, $params );
+						$event->result = $obj::$method( $event, $params );
 					} else {
-						$obj->$method( $event, $params );
+						$event->result = $obj->$method( $event, $params );
 					}
 				}
-
+				// Function
 				else if ( is_string($callback) ) {
-					$callback( $event, $params );
+					$event->result = $callback( $event, $params );
 				}
 
 				if ( !$event->_continue ) break;
@@ -81,19 +113,22 @@ class Event_Handler {
 	}
 }
 
+/**
+ * Event object that gets passed round robbin to the event listeners.
+ */
 class Event {
 	const BEFORE = 'BEFORE';
 	const AFTER  = 'AFTER';
 
-	public $name             = '';
+	public $name             = null;
 	public $data             = array();
-	public $folder           = '';
+	public $folder           = null;
 	public $result           = null;
 	public $_default         = true;
 	public $_continue        = true;
 	public $override_default = true;
 
-	public function __construct($name, &$data, $hooks_folder = '') {
+	public function __construct($name, &$data, $hooks_folder = null) {
 		$this->name   = $name;
 		$this->data   = $data;
 		$this->folder = $hooks_folder;
@@ -109,9 +144,21 @@ class Event {
 
 	/**
 	 * Prevent the event source from performing the deafault action.
+	 * Only works if the 'prevent_default' was enabled when the event triggered.
 	 */
 	public function prevent_default() {
 		$this->_default = false;
+	}
+
+	/**
+	 * Process the hooks of a given state. This can be used to run
+	 * custom states, to run BEFORE or AFTER states use the provided methods.
+	 * @param string $advise
+	 * @param boolean $override_default
+	 */
+	public function notify( $advise, $override_default = true ) {
+		$this->override_default = $override_default;
+		Event_Handler::process_event($this, $advise);
 	}
 
     /**
@@ -130,14 +177,15 @@ class Event {
      * @return  results of processing the event, usually $this->_default
      */
 	public function notify_before( $override_default = true) {
-		$this->override_default = $override_default;
-		Event_Handler::process_event($this, self::BEFORE);
+		$this->notify(self::BEFORE);
 		return (!$override_default || $this->_default);
 	}
 
+	/**
+	 * Notify hooks of event AFTER state.
+	 */
 	public function notify_after() {
-		$this->_continue = true;
-		Event_Handler::process_event($this, self::AFTER);
+		$this->notify(self::AFTER);
 	}
 
 	/**
@@ -155,27 +203,33 @@ class Event {
 	public function trigger( $callback = null, $override_default = true ) {
 		if ( !is_callable($callback) ) $override_default = false;
 
+		// BEFORE
 		if ( $this->notify_before($override_default) && is_callable($callback) ) {
+			// DEFAULT ACTION
 
+			// Anonymous function
 			if ( $callback instanceof Closure ) {
-				$callback( $this );
+				$this->result = $callback( $this );
 			}
-
+			// Object & Static methods
 			else if ( is_array($callback) ) {
 				list($obj, $method) = $callback;
+
 				if ( is_string($obj) ) {
-					$obj::$method( $this );
+					$this->result = $obj::$method( $$this );
 				} else {
-					$obj->$method( $this );
+					$this->result = $obj->$method( $this );
 				}
 			}
-
+			// Function
 			else if ( is_string($callback) ) {
-				$callback( $this );
+				$this->result = $callback( $event, $params );
 			}
 		}
 
+		// AFTER
 		$this->notify_after();
+		
 		return $this->result;
 	}
 }
@@ -194,8 +248,10 @@ class Event {
  *                                         it can be set or modified by event handler hooks
  */
 function trigger_event($name, $data, $action = null, $override_default = true) {
-	$event = new Event($name, $data);
-	return $event->trigger($action, $override_default);
+	$event  = new Event($name, $data);
+	$result = $event->trigger($action, $override_default);
+	unset( $event );
+	return $result;
 }
 
 /* End Events.php */
