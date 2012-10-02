99ab780c0e9e704e655cb3d3206ca83c78b3776c
Oz <osvaldo.lopez@superluminal.me>
2012-10-01 21:04:51 -0600

    Better hook loading.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,24 +2,12 @@
 
 class MY_Controller extends CI_Controller {
 
-	protected $hooks_folder = '';
+	protected $hooks_folder = null;
 
 	public function __construct() {
 		parent::__construct();
 
-		if ( !empty($this->hooks_folder) )
+		if ( is_string($this->hooks_folder) )
 			Event_Handler::load_hooks($this->hooks_folder);
 	}
-
-	/*
-	public function _remap($object_called, $arguments) {
-		// Go ahead and URL decode the arguments
-		$args = array();
-		foreach ( $arguments as $key => $arg ) {
-			$args[$key] = urldecode($arg);
-		}
-		
-		parent::_remap($object_called, $args);
-	}
-	*/
 }
